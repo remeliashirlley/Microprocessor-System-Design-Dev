730ef55a24a9a62ab9eec590013558fbb83c8987
remeliashirlley <83123538+remeliashirlley@users.noreply.github.com>
2021-12-01 15:48:28 +0800

    Add files via upload

---
+++ Switch/Debug/subdir_rules.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+LaunchPad.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LaunchPad.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=TARGET_IS_MSP432P4XX --define=ccs -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+%.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=TARGET_IS_MSP432P4XX --define=ccs -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
---
+++ Switches_LED/Debug/subdir_vars.mk
@@ -0,0 +1,57 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Clock.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/CortexM.c \
+../Lab08_LED_Switchesmain.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TExaS.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./Clock.d \
+./CortexM.d \
+./Lab08_LED_Switchesmain.d \
+./TExaS.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./Clock.obj \
+./CortexM.obj \
+./Lab08_LED_Switchesmain.obj \
+./TExaS.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"Clock.obj" \
+"CortexM.obj" \
+"Lab08_LED_Switchesmain.obj" \
+"TExaS.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"Clock.d" \
+"CortexM.d" \
+"Lab08_LED_Switchesmain.d" \
+"TExaS.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Clock.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/CortexM.c" \
+"../Lab08_LED_Switchesmain.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TExaS.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
