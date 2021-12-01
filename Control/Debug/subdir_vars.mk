5ee2974fef43ec6e97201811e73f4bba2df32890
remeliashirlley <83123538+remeliashirlley@users.noreply.github.com>
2021-12-01 15:55:14 +0800

    Add files via upload

---
+++ Control/Debug/subdir_vars.mk
@@ -0,0 +1,141 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/ADC14.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Bump.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Clock.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/CortexM.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/IRDistance.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LPF.c \
+../Lab17_Control.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LaunchPad.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Motor.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Nokia5110.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/PWM.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Reflectance.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/SysTickInts.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TA3InputCapture.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TExaS.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Tachometer.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TimerA1.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/UART0.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./ADC14.d \
+./Bump.d \
+./Clock.d \
+./CortexM.d \
+./IRDistance.d \
+./LPF.d \
+./Lab17_Control.d \
+./LaunchPad.d \
+./Motor.d \
+./Nokia5110.d \
+./PWM.d \
+./Reflectance.d \
+./SysTickInts.d \
+./TA3InputCapture.d \
+./TExaS.d \
+./Tachometer.d \
+./TimerA1.d \
+./UART0.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./ADC14.obj \
+./Bump.obj \
+./Clock.obj \
+./CortexM.obj \
+./IRDistance.obj \
+./LPF.obj \
+./Lab17_Control.obj \
+./LaunchPad.obj \
+./Motor.obj \
+./Nokia5110.obj \
+./PWM.obj \
+./Reflectance.obj \
+./SysTickInts.obj \
+./TA3InputCapture.obj \
+./TExaS.obj \
+./Tachometer.obj \
+./TimerA1.obj \
+./UART0.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"ADC14.obj" \
+"Bump.obj" \
+"Clock.obj" \
+"CortexM.obj" \
+"IRDistance.obj" \
+"LPF.obj" \
+"Lab17_Control.obj" \
+"LaunchPad.obj" \
+"Motor.obj" \
+"Nokia5110.obj" \
+"PWM.obj" \
+"Reflectance.obj" \
+"SysTickInts.obj" \
+"TA3InputCapture.obj" \
+"TExaS.obj" \
+"Tachometer.obj" \
+"TimerA1.obj" \
+"UART0.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"ADC14.d" \
+"Bump.d" \
+"Clock.d" \
+"CortexM.d" \
+"IRDistance.d" \
+"LPF.d" \
+"Lab17_Control.d" \
+"LaunchPad.d" \
+"Motor.d" \
+"Nokia5110.d" \
+"PWM.d" \
+"Reflectance.d" \
+"SysTickInts.d" \
+"TA3InputCapture.d" \
+"TExaS.d" \
+"Tachometer.d" \
+"TimerA1.d" \
+"UART0.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/ADC14.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Bump.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Clock.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/CortexM.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/IRDistance.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LPF.c" \
+"../Lab17_Control.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LaunchPad.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Motor.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Nokia5110.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/PWM.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Reflectance.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/SysTickInts.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TA3InputCapture.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TExaS.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Tachometer.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TimerA1.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/UART0.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
---
+++ Flash/Debug/subdir_vars.mk
@@ -0,0 +1,69 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Clock.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/FlashProgram.c \
+../FlashTestMain.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LaunchPad.c \
+../Scoreboard.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/UART0.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./Clock.d \
+./FlashProgram.d \
+./FlashTestMain.d \
+./LaunchPad.d \
+./Scoreboard.d \
+./UART0.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./Clock.obj \
+./FlashProgram.obj \
+./FlashTestMain.obj \
+./LaunchPad.obj \
+./Scoreboard.obj \
+./UART0.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"Clock.obj" \
+"FlashProgram.obj" \
+"FlashTestMain.obj" \
+"LaunchPad.obj" \
+"Scoreboard.obj" \
+"UART0.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"Clock.d" \
+"FlashProgram.d" \
+"FlashTestMain.d" \
+"LaunchPad.d" \
+"Scoreboard.d" \
+"UART0.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Clock.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/FlashProgram.c" \
+"../FlashTestMain.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LaunchPad.c" \
+"../Scoreboard.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/UART0.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
