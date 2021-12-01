a9caf6c4858b7e432b7571020e6fd4f95cf489b2
remeliashirlley <83123538+remeliashirlley@users.noreply.github.com>
2021-12-01 15:49:47 +0800

    Add files via upload

---
+++ Lab5_UARTComms/Debug/subdir_rules.mk
@@ -0,0 +1,125 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+ADC14.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/ADC14.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+BumpInt.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/BumpInt.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+Clock.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Clock.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+CortexM.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/CortexM.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+EUSCIA0.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/EUSCIA0.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+FIFO0.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/FIFO0.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+IRDistance.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/IRDistance.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+LPF.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LPF.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+%.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+LaunchPad.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LaunchPad.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+Motor.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Motor.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+PWM.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/PWM.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+Reflectance.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Reflectance.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+SysTickInts.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/SysTickInts.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+TA3InputCapture.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TA3InputCapture.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+TimerA1.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TimerA1.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+UART0.obj: /Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/UART0.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP432 Compiler'
+	"/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include" --include_path="/Applications/ti/ccs1040/ccs/ccs_base/arm/include/CMSIS" --include_path="/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/Lab5_UARTComms" --include_path="/Applications/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --c99 --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
---
+++ Lab5_UARTComms/Debug/subdir_vars.mk
@@ -0,0 +1,135 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/ADC14.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/BumpInt.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Clock.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/CortexM.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/EUSCIA0.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/FIFO0.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/IRDistance.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LPF.c \
+../Lab5_UARTmain.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LaunchPad.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Motor.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/PWM.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Reflectance.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/SysTickInts.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TA3InputCapture.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TimerA1.c \
+/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/UART0.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./ADC14.d \
+./BumpInt.d \
+./Clock.d \
+./CortexM.d \
+./EUSCIA0.d \
+./FIFO0.d \
+./IRDistance.d \
+./LPF.d \
+./Lab5_UARTmain.d \
+./LaunchPad.d \
+./Motor.d \
+./PWM.d \
+./Reflectance.d \
+./SysTickInts.d \
+./TA3InputCapture.d \
+./TimerA1.d \
+./UART0.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./ADC14.obj \
+./BumpInt.obj \
+./Clock.obj \
+./CortexM.obj \
+./EUSCIA0.obj \
+./FIFO0.obj \
+./IRDistance.obj \
+./LPF.obj \
+./Lab5_UARTmain.obj \
+./LaunchPad.obj \
+./Motor.obj \
+./PWM.obj \
+./Reflectance.obj \
+./SysTickInts.obj \
+./TA3InputCapture.obj \
+./TimerA1.obj \
+./UART0.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"ADC14.obj" \
+"BumpInt.obj" \
+"Clock.obj" \
+"CortexM.obj" \
+"EUSCIA0.obj" \
+"FIFO0.obj" \
+"IRDistance.obj" \
+"LPF.obj" \
+"Lab5_UARTmain.obj" \
+"LaunchPad.obj" \
+"Motor.obj" \
+"PWM.obj" \
+"Reflectance.obj" \
+"SysTickInts.obj" \
+"TA3InputCapture.obj" \
+"TimerA1.obj" \
+"UART0.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"ADC14.d" \
+"BumpInt.d" \
+"Clock.d" \
+"CortexM.d" \
+"EUSCIA0.d" \
+"FIFO0.d" \
+"IRDistance.d" \
+"LPF.d" \
+"Lab5_UARTmain.d" \
+"LaunchPad.d" \
+"Motor.d" \
+"PWM.d" \
+"Reflectance.d" \
+"SysTickInts.d" \
+"TA3InputCapture.d" \
+"TimerA1.d" \
+"UART0.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/ADC14.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/BumpInt.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Clock.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/CortexM.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/EUSCIA0.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/FIFO0.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/IRDistance.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LPF.c" \
+"../Lab5_UARTmain.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/LaunchPad.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Motor.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/PWM.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/Reflectance.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/SysTickInts.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TA3InputCapture.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/TimerA1.c" \
+"/Users/remeliashirlley/Desktop/Courses/Y2/CE2107/CE2007-Students/inc/UART0.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
